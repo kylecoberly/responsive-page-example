c140f4d9086e790132ccc1fa453935f724c4e689
Kyle Coberly <kyle.coberly@gmail.com>
2018-06-18 14:20:48 -0600

    Adding blurb

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, {Component} from "react";
 import Hero from "./components/hero/component.js";
+import Blurb from "./components/blurb/component.js";
 
 import image from "./assets/images/bedroom.jpg";
 const title = "Faye’s Classic Eclectic Living Room";
 const label = "Case Study";
+const copy = "This is a small intro blurb that gives you just a bit of insight into Faye’s project. Should you decide to read more, than you scroll down to get the.";
 
 export default class App extends Component {
     render(){
@@ -14,6 +16,7 @@ export default class App extends Component {
                     title={title}
                     label={label}
                 />
+                <Blurb copy={copy} />
             </div>
         )
     }
---
+++ src/components/blurb/component.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+import "./style.css";
+
+export default class Blurb extends Component {
+    render(){
+        return (
+            <div className="blurb">
+                <p>{this.props.copy}</p>
+            </div>
+        )
+    }
+}
